866f7a8279eb7bf436ebcd7b400053c8908f2df5
rintarouchida <ww.stamford@gmail.com>
2023-07-02 16:14:25 +0900

    feat: refs feat_request_test

---
+++ backend/laravel/tests/Unit/app/Http/Requests/Party/RegisterRequestTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Tests\Unit\app\Http\Requests\Party;
+
+use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\Party\RegisterRequest;
+use Tests\TestCase;
+
+class RegisterRequestTest extends TestCase
+{
+    /**
+     * @dataProvider validationProvider
+     * @return void
+     */
+    public function testValidation($inData, $outFail, $outMessage)
+    {
+        $request = new RegisterRequest();
+        $rules = $request->rules();
+        $messages = $request->messages();
+        $validator = Validator::make($inData, $rules, $messages);
+        $result = $validator->fails();
+        $this->assertEquals($outFail, $result);
+        $messages = $validator->errors()->getMessages();
+        $this->assertEquals($outMessage, $messages);
+    }
+
+    public function validationProvider()
+    {
+        return [
+            '正常' => [
+                [
+                    'pref_id' => 1,
+                    'theme' => str_repeat('a', 30),
+                    'introduction' => '詳細1',
+                    'place' => '場所1',
+                    'due_max' => 2,
+                    'due_date' => 2,
+                ],
+                false,
+                [],
+            ],
+            '異常 必須項目未入力' => [
+                [],
+                true,
+                [
+                    'pref_id' => ['都道府県を選択してください。'],
+                    'theme' => ['題名を入力してください。'],
+                    'introduction' => ['詳細を入力してください。'],
+                    'place' => ['開催場所を入力してください。'],
+                    'due_max' => ['定員を選択してください。'],
+                    'due_date' => ['締切を入力してください。'],
+                ],
+            ],
+            '異常 異なる型' => [
+                [
+                    'pref_id' => 1,
+                    'theme' => 1,
+                    'introduction' => '詳細1',
+                    'place' => '場所1',
+                    'due_max' => 2,
+                    'due_date' => 2,
+                ],
+                true,
+                [
+                    'theme' => ['題名は文字列で入力してください。'],
+                ],
+            ],
+            '異常 文字数オーバー' => [
+                [
+                    'pref_id' => 1,
+                    'theme' => str_repeat('a', 31),
+                    'introduction' => '詳細1',
+                    'place' => '場所1',
+                    'due_max' => 2,
+                    'due_date' => 2,
+                ],
+                true,
+                [
+                    'theme' => ['題名は30文字以内で入力してください。'],
+                ],
+            ],
+        ];
+    }
+}
