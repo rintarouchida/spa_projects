be16b1a83b2be54f71ac9e69883821bf94fd370d
rintarouchida <ww.stamford@gmail.com>
2023-07-03 20:32:55 +0900

    Serviceクラステスト修正

--- backend/laravel/tests/Unit/app/Services/PartyServiceTest.php
+++ backend/laravel/tests/Unit/app/Services/PartyServiceTest.php
@@ -7,11 +7,21 @@ use App\Models\Pref;
 use App\Models\User;
 use App\Models\Tag;
 use App\Services\PartyService;
+use DB;
 use Illuminate\Database\Eloquent\Factories\Sequence;
 use Tests\TestCase;
 
 class PartyServiceTest extends TestCase
 {
+    protected User $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory(['id' => 1, 'name' => 'ユーザー1'])->create();
+    }
+
     /**
      * A basic unit test example.
      *
@@ -29,7 +39,6 @@ class PartyServiceTest extends TestCase
             'leader_id' => 1,
         ]);
 
-        $user = User::factory(['id' => 1])->create();
         Pref::factory(['id' => 1])->create();
         $this->assertTrue(true);
         $data = [
@@ -44,7 +53,7 @@ class PartyServiceTest extends TestCase
         ];
 
         $service = new PartyService();
-        $service->register($data, $user->id);
+        $service->register($data, $this->user->id);
 
         $this->assertDatabaseHas('parties', [
             'theme' => 'theme_1',
@@ -66,7 +75,6 @@ class PartyServiceTest extends TestCase
     public function test_getData()
     {
         Pref::factory(['id' => 1])->create();
-        User::factory(['id' => 1, 'name' => 'ユーザー1'])->create();
         Tag::factory(3)->create(new Sequence(
             ['id' => 1, 'name' => 'tag_1'],
             ['id' => 2, 'name' => 'tag_2'],
@@ -74,7 +82,7 @@ class PartyServiceTest extends TestCase
         ));
         Party::factory([
             'id' => 1,
-            'leader_id' => 1,
+            'leader_id' => $this->user->id,
             'place' => '東京都港区',
             'theme' => 'テストパーティー1',
             'pref_id' => 1,
@@ -96,5 +104,62 @@ class PartyServiceTest extends TestCase
             'due_date' => '2023-05-08',
             'tags' => ['tag_1', 'tag_2', 'tag_3']
         ]);
+    }
+
+    /**
+     * join
+     *
+     * @return void
+     */
+    public function test_join()
+    {
+        User::factory(['id' => 2])->create();
+        Pref::factory(['id' => 1])->create();
+        $party = Party::factory([
+            'id' => 1,
+            'leader_id' => 2,
+            'place' => '東京都港区',
+            'theme' => 'テストパーティー1',
+            'pref_id' => 1,
+            'due_max' => 10,
+            'due_date' => '2023-05-08 00:00:00',
+            'introduction' => '詳細1',
+        ])->create();
+
+        $this->assertFalse(DB::table('party_user')->where('party_id', $party->id)->where('user_id', $this->user->id)->exists());
+
+        $this->actingAs($this->user);
+        $service = new PartyService();
+        $service->join($party->id);
+        $this->assertTrue(DB::table('party_user')->where('party_id', $party->id)->where('user_id', $this->user->id)->exists());
+    }
+
+    /**
+     * checkIfJoined
+     *
+     * @return void
+     */
+    public function test_checkIfJoined()
+    {
+        User::factory(['id' => 2])->create();
+        Pref::factory(['id' => 1])->create();
+        $party = Party::factory([
+            'id' => 1,
+            'leader_id' => 2,
+            'place' => '東京都港区',
+            'theme' => 'テストパーティー1',
+            'pref_id' => 1,
+            'due_max' => 10,
+            'due_date' => '2023-05-08 00:00:00',
+            'introduction' => '詳細1',
+        ])->create();
+
+        $this->actingAs($this->user);
+        $service = new PartyService();
+
+        $this->assertFalse($service->checkIfJoined($party->id));
+
+        $this->user->parties()->attach($party->id);
+        $this->assertTrue($service->checkIfJoined($party->id));
     }
 }
