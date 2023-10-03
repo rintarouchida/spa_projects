4e27590598e7d6d6e02de826bf8578b7051ea459
rintarouchida <ww.stamford@gmail.com>
2023-10-03 23:24:44 +0900

    feat:messagesテーブル作成

---
+++ backend/laravel/database/migrations/2023_10_03_232202_create_messages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('message_group_id');
+            $table->string('content');
+            $table->timestamps();
+
+            $table->foreign('message_group_id')->references('id')->on('message_groups');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
