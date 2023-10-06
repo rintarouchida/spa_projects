0c75120ea4ae1cb84ea2ca2b63aa327a096790e0
rintarouchida <ww.stamford@gmail.com>
2023-10-06 23:04:53 +0900

    feat:send_message_request

--- backend/laravel/app/Http/Requests/Message/SendMessageRequest.php
+++ backend/laravel/app/Http/Requests/Message/SendMessageRequest.php
@@ -13,7 +13,7 @@ class SendMessageRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,7 @@ class SendMessageRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'content' => 'required|string|max:255',
         ];
     }
 }
