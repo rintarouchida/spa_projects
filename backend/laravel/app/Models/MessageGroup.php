60c8b4d547de7687881fb33b64ad8cedd5480b27
rintarouchida <ww.stamford@gmail.com>
2023-10-09 14:14:37 +0900

    メッセージ機能修正

--- backend/laravel/app/Models/MessageGroup.php
+++ backend/laravel/app/Models/MessageGroup.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,4 +10,9 @@ class MessageGroup extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'user_message_group');
+    }
 }
