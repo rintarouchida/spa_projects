760903624f951c5598ca8b38a4c7a8cbc0e44b2e
rintarouchida <ww.stamford@gmail.com>
2023-10-27 00:01:16 +0900

    もくもく会検索API(タグ検索機能)作成

--- backend/laravel/app/Services/Search/PartyService.php
+++ backend/laravel/app/Services/Search/PartyService.php
@@ -15,7 +15,6 @@ class PartyService
     */
     public function searchParties(array $params): array
     {
-        //todo:もくもく会検索機能作成
         $sevendays=Carbon::today()->subDay(7);
         $query = Party::with('tags')->whereDate('created_at', '>=', $sevendays);
 
@@ -28,6 +27,16 @@ class PartyService
         if(!is_null($params['keyword'])) {
             $query = $this->fetchRecordByTagId($query, $params['keyword']);
         }
+        foreach ($query as $key => $party) {
+            $data[$key]['id'] = $party->id;
+            $data[$key]['theme'] = $party->theme;
+            $data[$key]['place'] = $party->place;
+            $data[$key]['due_max'] = $party->due_max;
+            foreach($party->tags as $index => $tag) {
+                $data[$key]['tags'][$index]['name'] = $tag->name;
+            }
+        }
+        return $data;
     }
 
     /**
@@ -37,8 +46,8 @@ class PartyService
      * @return Builder
      */
     protected function fetchRecordByKeyword(Builder $query, string $keyword): Builder
-    {
-        //todo:文字列検索の条件洗い出し
+
+        //文字列検索の条件洗い出し
         // →文字列がthemeに入る
         $query->where('theme', 'like', '%'.$keyword.'%');
         // →文字列がplaceに入る
@@ -49,10 +58,14 @@ class PartyService
         return $query;
     }
 
-    protected function fetchRecordByTagId($query, $tag_ids)
+    protected function fetchRecordByTagId(Builder $query, array $tag_ids): Builder
     {
-        //todo: タグID条件の洗い出し
+        //タグID条件の洗い出し
         //    →tag_idsのどれか一つをもつレコードを全て取り出す
         //    →(例)tag_ids=[1, 2, 3]とすると、[3, 4, 5]に紐ずくレコードは3が合致するので抽出される
+        $query->('tags', function ($q) use ($tag_ids) {
+            $q->whereIn('id', $tag_ids);
+        });
+        return $query
     }
 }
