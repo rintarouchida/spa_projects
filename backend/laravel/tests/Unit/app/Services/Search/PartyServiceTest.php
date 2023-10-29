7e9f24a65b900dc433f3a5871484382cc3af4926
rintarouchida <ww.stamford@gmail.com>
2023-10-29 19:12:45 +0900

    ユニットテスト作成

--- backend/laravel/tests/Unit/app/Services/Search/PartyServiceTest.php
+++ backend/laravel/tests/Unit/app/Services/Search/PartyServiceTest.php
@@ -79,4 +79,43 @@ class PartyServiceTest extends TestCase
         $actual = $method->invoke(new PartyService, Party::query(), '山田');
         $this->assertSame([1, 2, 3], $actual->get()->pluck('id')->toArray());
     }
+
+    /**
+     * searchParties
+     *
+     * @return void
+     */
+    public function test_search_parties(): void
+    {
+        Carbon::setTestNow('2023-10-29 10:00:00');
+
+        Pref::factory(['id' => 1])->create();
+        Pref::factory(['id' => 2])->create();
+
+        Tag::factory(4)->create(new Sequence(
+            ['id' => 1, 'name' => 'タグ1'],
+            ['id' => 2, 'name' => 'タグ2'],
+            ['id' => 3, 'name' => 'タグ3'],
+            ['id' => 4, 'name' => 'タグ4']
+        ));
+
+        Party::factory(['id' => 1, 'theme' => 'theme_1', 'pref_id' => 1, 'place' => 'place_1', 'due_max' => 1, 'created_at' => '2023-10-29 10:00:00'])->create()->tags()->attach([1,2]);
+
+        $data = [
+            'pref_id'  => 1,
+            'tag_id'   => [2, 3],
+            'keyword' => 'place_1'
+        ];
+
+        $service = new PartyService();
+
+        $actual = $service->searchParties($data);
+        $this->assertSame([
+            ['id' => 1, 'theme' => 'theme_1', 'place' => 'place_1', 'due_max' => 1,
+            'tags' => [
+                ['name' => 'タグ1'],
+                ['name' => 'タグ2']
+            ]],
+        ], $actual);
+    }
 }
