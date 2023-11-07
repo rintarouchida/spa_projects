28558159a059a45b1ed8eb8d23c26b5966dbe773
rintarouchida <ww.stamford@gmail.com>
2023-11-07 20:38:39 +0900

    パスワード再設定画面作成(パスワード送信)

--- frontend/pages/auth/edit_password/_token.vue
+++ frontend/pages/auth/edit_password/_token.vue
@@ -1,7 +1,25 @@
 <template>
-  <div>
-    <h1>パスワード再設定</h1>
-    <p>{{ token }}</p>
+  <div style="text-align: center">
+    <div class="password_reset_form">
+      <h1>パスワード再設定画面</h1>
+      <p>新しいパスワードを入力してください。</p>
+      <h3 class="password_reset_items">パスワード</h3>
+      <input
+        v-model="password"
+        class="input_form"
+        type="password"
+        placeholder="パスワード"
+      />
+      <h3 class="password_reset_items">パスワード(確認用)</h3>
+      <input
+        v-model="password_confirm"
+        class="input_form"
+        type="password"
+        placeholder="パスワード"
+      />
+      <v-btn color="red" style="color: white">送信する</v-btn
+      >
+    </div>
   </div>
 </template>
 
@@ -12,10 +30,32 @@ export default {
   data() {
     return {
       token: this.$route.params.token,
+      email: this.$route.query.email,
+      password: "",
+      password_confirm: "",
     }
   },
 }
 </script>
 
-<style>
+<style scoped>
+.password_reset_form {
+  margin-top: 20px;
+  margin-left: 15%;
+  width: 70%;
+  padding-left: 5%;
+  padding-right: 5%;
+  background-color: #e8e6e6;
+  padding-bottom: 20px;
+}
+.password_reset_items {
+  text-align: left;
+}
+.input_form {
+  border: 1px solid black;
+  background-color: white;
+  width: 100%;
+  height: 40px;
+  margin-bottom: 20px;
+}
 </style>
