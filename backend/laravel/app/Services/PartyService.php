fe0f0a390549dd82b7e6b147f85ff3292e4c367d
rintarouchida <ww.stamford@gmail.com>
2023-11-15 21:23:56 +0900

    ユニットテストの不具合修正

--- backend/laravel/app/Services/PartyService.php
+++ backend/laravel/app/Services/PartyService.php
@@ -45,7 +45,6 @@ class PartyService
         $data['place'] = $party->place;
         $data['due_max'] = $party->due_max;
         $data['user_name'] = $party->leader->name;
-        //todo:$data['user_id'] = $party->leader->id;を追加したのでテストに反映
         $data['user_id'] = $party->leader->id;
         $data['introduction'] = $party->introduction;
         $data['due_date'] = $party->due_date;
