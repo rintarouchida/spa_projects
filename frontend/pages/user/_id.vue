754128a2d4689bddba020673423d043257f10649
rintarouchida <ww.stamford@gmail.com>
2023-11-16 20:01:51 +0900

    ユーザー詳細ページ作成

--- frontend/pages/user/_id.vue
+++ frontend/pages/user/_id.vue
@@ -1,16 +1,47 @@
 <template>
   <div>
-    <h1>ユーザー詳細ページ</h1>
-    <!-- todo:デザイン作成 -->
+    <h2>{{ user.name }}さんのプロフィール</h2>
+    <div style="width: 30%; float: left">
+      <p class="picture"></p>
+    </div>
+    <div style="width: 70%; float: right">
+      <h2 style="margin-top: 15px">{{ user.name }}さん</h2>
+      <p style="margin-top: 15px">{{ user.old }}才</p>
+      <p style="margin-top: 15px">{{ user.pref_name }}在住</p>
+    </div>
+    <div class="clear"></div>
+    <p>{{ user.introduction }}</p>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      user: '',
+    }
+  },
+  async created() {
+    setTimeout(() => this.$nuxt.$loading.start(), 500)
 
+    this.user = await this.$axios
+      .get(`api/user/get/${this.$route.params.id}`)
+      .then((res) => {
+        return res.data
+      })
+
+    this.$nuxt.$loading.finish()
+  },
 }
 </script>
 
 <style>
-
+.picture {
+  background-color: yellow;
+  width: 100%;
+  height: 200px;
+}
+.clear {
+  clear: both;
+}
 </style>
