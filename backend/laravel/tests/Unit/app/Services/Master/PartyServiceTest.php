6b5f338b57ca1fdd4f3d31b75fc286fc5b01e428
rintarouchida <ww.stamford@gmail.com>
2023-11-19 23:02:07 +0900

    リファクタリング

--- backend/laravel/tests/Unit/app/Services/Master/PartyServiceTest.php
+++ backend/laravel/tests/Unit/app/Services/Master/PartyServiceTest.php
@@ -11,7 +11,6 @@ use Illuminate\Database\Eloquent\Factories\Sequence;
 
 class PartyServiceTest extends TestCase
 {
-    //todo:テスト修正
     /**
      * fetchPickUpParties
      *
@@ -20,21 +19,24 @@ class PartyServiceTest extends TestCase
     public function test_fetch_pick_up_parties()
     {
         Carbon::setTestNow('2023-05-20 10:00:00');
+        $user = User::factory(['id' => 1])->create();
+        User::factory(['id' => 2])->create();
         Party::factory(5)->create(new Sequence(
-            ['id' => 1, 'theme' => 'theme_1', 'place' => 'place_1', 'due_max' => 1, 'created_at' => '2023-05-19 10:00:00'],
-            ['id' => 2, 'theme' => 'theme_2', 'place' => 'place_2', 'due_max' => 2, 'created_at' => '2023-05-17 10:00:00'],
-            ['id' => 3, 'theme' => 'theme_3', 'place' => 'place_3', 'due_max' => 3, 'created_at' => '2023-05-15 10:00:00'],
-            ['id' => 4, 'theme' => 'theme_4', 'place' => 'place_4', 'due_max' => 4, 'created_at' => '2023-05-14 10:00:00'],
-            ['id' => 5, 'theme' => 'theme_5', 'place' => 'place_5', 'due_max' => 5, 'created_at' => '2023-05-12 10:00:00'],
+            ['id' => 1, 'theme' => 'theme_1', 'place' => 'place_1', 'due_max' => 1, 'created_at' => '2023-05-19 10:00:00', 'leader_id' => 1],
+            ['id' => 2, 'theme' => 'theme_2', 'place' => 'place_2', 'due_max' => 2, 'created_at' => '2023-05-17 10:00:00', 'leader_id' => 2],
+            ['id' => 3, 'theme' => 'theme_3', 'place' => 'place_3', 'due_max' => 3, 'created_at' => '2023-05-15 10:00:00', 'leader_id' => 2],
+            ['id' => 4, 'theme' => 'theme_4', 'place' => 'place_4', 'due_max' => 4, 'created_at' => '2023-05-14 10:00:00', 'leader_id' => 2],
+            ['id' => 5, 'theme' => 'theme_5', 'place' => 'place_5', 'due_max' => 5, 'created_at' => '2023-05-12 10:00:00', 'leader_id' => 2],
         ));
+
+        $user->parties()->attach(3);
 
         $service = new PartyService();
 
-        $actual = $service->fetchPickUpParties();
+        $actual = $service->fetchPickUpParties(1);
+        //(登録から1週間以内)かつ(自身が作成したものでない)かつ(自身がまだ参加していない)もくもく会が抽出される
         $this->assertSame($actual, [
-            ['id' => 1, 'theme' => 'theme_1', 'place' => 'place_1', 'due_max' => 1],
             ['id' => 2, 'theme' => 'theme_2', 'place' => 'place_2', 'due_max' => 2],
-            ['id' => 3, 'theme' => 'theme_3', 'place' => 'place_3', 'due_max' => 3],
             ['id' => 4, 'theme' => 'theme_4', 'place' => 'place_4', 'due_max' => 4],
         ]);
     }
