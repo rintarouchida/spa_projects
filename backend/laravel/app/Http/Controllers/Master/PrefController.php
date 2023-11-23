7b239f82a5390ee6bcdf290d655ae5aaab57a324
rintarouchida <ww.stamford@gmail.com>
2023-11-23 23:06:05 +0900

    リファクタリング

--- backend/laravel/app/Http/Controllers/Master/PrefController.php
+++ backend/laravel/app/Http/Controllers/Master/PrefController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers\Master;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Pref;
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class PrefController extends Controller
 {
