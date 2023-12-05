b3d6eac2f8968f7e9caa347f6809cc24b05a8407
rintarouchida <ww.stamford@gmail.com>
2023-12-05 20:40:28 +0900

    ユーザー情報更新機能作成

--- backend/laravel/app/Services/UserService.php
+++ backend/laravel/app/Services/UserService.php
@@ -14,13 +14,30 @@ class UserService
      */
     public function getdata(int $user_id): array
     {
+        //todo:テスト修正
         $user = User::find($user_id);
         $data = [];
+        $data['id'] = $user->id;
         $data['name'] = $user->name;
+        $data['email'] = $user->email;
+        $data['pref_id'] = $user->pref_id;
+        $data['birthday'] = Carbon::createFromFormat('Y-m-d H:i:s', $user->birthday)->format('Y-m-d');
+        $data['introduction'] = $user->introduction;
+        $data['twitter_url'] = $user->twitter_url;
         $data['old']  = Carbon::parse($user->birthday)->age;
         $data['pref_name'] = $user->pref->name;
-        $data['introduction'] = $user->introduction;
 
         return $data;
+    }
+
+    //todo:ユーザー更新関数作る
+    /**
+     * @param array $data
+     *
+     * @return void
+     */
+    public function update(User $user, array $data): void
+    {
+        $user->fill($data)->save();
     }
 }
