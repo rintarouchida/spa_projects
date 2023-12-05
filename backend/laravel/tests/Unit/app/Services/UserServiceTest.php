33a757c077e27d5a2480e902dcdfa8fbe18d05e3
rintarouchida <ww.stamford@gmail.com>
2023-12-06 00:03:14 +0900

    リファクタリング

--- backend/laravel/tests/Unit/app/Services/UserServiceTest.php
+++ backend/laravel/tests/Unit/app/Services/UserServiceTest.php
@@ -7,6 +7,7 @@ use App\Models\Pref;
 use App\Services\UserService;
 use Tests\TestCase;
 use Carbon\Carbon;
+use ReflectionMethod;
 
 class UserServiceTest extends TestCase
 {
@@ -17,12 +18,15 @@ class UserServiceTest extends TestCase
         parent::setUp();
         Carbon::setTestNow('2023-11-15 00:00:00');
         Pref::factory(['id' => 1, 'name' => 'pref_1'])->create();
+        Pref::factory(['id' => 2, 'name' => 'pref_2'])->create();
         $this->user = User::factory([
             'id'           => 1,
             'name'         => 'ユーザー1',
+            'email'        => 'test1@gmail.com',
             'birthday'     => '2000-01-01 00:00:00',
             'introduction' => 'introduction1',
             'pref_id'      => 1,
+            'twitter_url'  => 'https://twitter.com',
         ])->create();
     }
 
@@ -37,10 +41,44 @@ class UserServiceTest extends TestCase
 
         $actual = $service->getData($this->user->id);
         $this->assertSame($actual, [
+            'id'           => 1,
             'name'         => 'ユーザー1',
+            'email'        => 'test1@gmail.com',
+            'pref_id'      => 1,
+            'birthday'     => '2000-01-01',
+            'introduction' => 'introduction1',
+            'twitter_url'  => 'https://twitter.com',
             'old'          => 23,
             'pref_name'    => 'pref_1',
+
+        ]);
+    }
+
+    /**
+     * update
+     *
+     * @return void
+     */
+    public function test_update()
+    {
+        $data = [
+            'name'         => 'ユーザー2',
+            'email'        => 'test2@gmail.com',
+            'pref_id'      => 2,
+        ];
+
+        $method = new ReflectionMethod(UserService::class, 'update');
+        $method->setAccessible(true);
+        $method->invoke(new UserService, $this->user, $data);
+
+        $this->assertDatabaseHas('users', [
+            'id'           => 1,
+            'name'         => 'ユーザー2',
+            'email'        => 'test2@gmail.com',
+            'birthday'     => '2000-01-01 00:00:00',
             'introduction' => 'introduction1',
+            'pref_id'      => 2,
+            'twitter_url'  => 'https://twitter.com',
         ]);
     }
 }
