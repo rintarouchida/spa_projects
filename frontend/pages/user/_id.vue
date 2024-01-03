21f19ad02b89289ccd3ed02133541305d42c3f65
rintarouchida <ww.stamford@gmail.com>
2024-01-04 03:53:50 +0900

    ユーザー情報部分のデザイン修正、レスポンシブ対応

--- frontend/pages/user/_id.vue
+++ frontend/pages/user/_id.vue
@@ -2,15 +2,22 @@
   <div>
     <h2>{{ user.name }}さんのプロフィール</h2>
     <div style="width: 30%; float: left">
-      <p class="picture"></p>
+      <p class="picture">
+        <img
+          :src="user.image"
+          alt=""
+          style="display: block; width: 100%; height: 100%"
+        />
+      </p>
     </div>
-    <div style="width: 70%; float: right">
+    <div style="width: 70%; padding-left:10%; float: right">
       <h2 style="margin-top: 15px">{{ user.name }}さん</h2>
-      <p style="margin-top: 15px">{{ user.old }}才</p>
-      <p style="margin-top: 15px">{{ user.pref_name }}在住</p>
+      <p style="margin-top: 15px">(年齢){{ user.old }}才</p>
+      <p style="margin-top: 15px">(在住){{ user.pref_name }}</p>
+      <p>(TwitterのURL)<a :href="user.twitter_url">{{ user.twitter_url }}</a></p>
     </div>
     <div class="clear"></div>
-    <p>{{ user.introduction }}</p>
+    <p>(自己紹介)<br>{{ user.introduction }}</p>
   </div>
 </template>
 
@@ -47,5 +54,12 @@ export default {
 }
 .clear {
   clear: both;
+}
+
+@media (max-width: 600px) {
+  .picture{
+    height: 150px;
+    width: 150px;
+  }
 }
 </style>
