422d0e5e244c4f9a680f18b3cb1223bcd05dafbc
rintarouchida <ww.stamford@gmail.com>
2024-01-04 03:31:28 +0900

    スマホ用のヘッダー作成

--- frontend/components/NewHeader.vue
+++ frontend/components/NewHeader.vue
@@ -1,31 +1,54 @@
 <template>
-  <header class="header" @mouseleave="menu1 = false; menu2 = false; menu3 = false">
-    <nav>
-      <div class="dropdown">
-        <button @mouseover="menu1 = !menu1; menu2 = false; menu3 = false">もくもくMAP</button>
-        <div v-if="menu1" class="menu">
-          <p><router-link class="link" to="/">一覧画面へ</router-link></p>
-          <p><router-link class="link" to="/create_party">もくもく会を作成</router-link></p>
-          <p><router-link class="link" to="/party/created_list">作成したもくもく会一覧</router-link></p>
-          <p><router-link class="link" to="/party/participated_list">参加したもくもく会一覧</router-link></p>
+  <div>
+    <header class="header" @mouseleave="menu1 = false; menu2 = false; menu3 = false">
+      <nav>
+        <div class="dropdown">
+          <button @mouseover="menu1 = !menu1; menu2 = false; menu3 = false">もくもくMAP</button>
+          <div v-if="menu1" class="menu">
+            <p><router-link class="link" to="/">一覧画面へ</router-link></p>
+            <p><router-link class="link" to="/create_party">もくもく会を作成</router-link></p>
+            <p><router-link class="link" to="/party/created_list">作成したもくもく会一覧</router-link></p>
+            <p><router-link class="link" to="/party/participated_list">参加したもくもく会一覧</router-link></p>
+          </div>
         </div>
-      </div>
-      <div class="dropdown">
-        <button @mouseover="menu2 = !menu2; menu1 = false; menu3 = false">メッセージ</button>
-        <div v-if="menu2" class="menu">
-          <p><router-link class="link" to="/messages">メッセージ(参加者側)</router-link></p>
-          <p><router-link class="link" to="/messages_for_leader">メッセージ(主催者側)</router-link></p>
+        <div class="dropdown">
+          <button @mouseover="menu2 = !menu2; menu1 = false; menu3 = false">メッセージ</button>
+          <div v-if="menu2" class="menu">
+            <p><router-link class="link" to="/messages">メッセージ(参加者側)</router-link></p>
+            <p><router-link class="link" to="/messages_for_leader">メッセージ(主催者側)</router-link></p>
+          </div>
         </div>
-      </div>
-      <div class="dropdown">
-        <button @mouseover="menu3 = !menu3; menu1 = false; menu2 = false">ユーザー</button>
-        <div v-if="menu3" class="menu">
-          <p><router-link class="link" to="/user/show">ユーザー情報</router-link></p>
-          <button class="link" @click="logout" style="margin-top:5px;">ログアウト</button>
+        <div class="dropdown">
+          <button @mouseover="menu3 = !menu3; menu1 = false; menu2 = false">ユーザー</button>
+          <div v-if="menu3" class="menu">
+            <p><router-link class="link" to="/user/show">ユーザー情報</router-link></p>
+            <button class="link" @click="logout" style="margin-top:5px;">ログアウト</button>
+          </div>
         </div>
+      </nav>
+    </header>
+    <header class="header_sp">
+      <div class="hamburger-menu" @click="toggleMenu">
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
       </div>
-    </nav>
-  </header>
+    </header>
+        <!-- ハンバーガーメニューのコンテンツ -->
+    <div class="menu-content" id="menuContent">
+      <!-- メニューアイテム -->
+      <p></p>
+      <p></p>
+      <p><a class="link" @click="toRoute('/')">一覧画面へ</a></p>
+      <p><a class="link" @click="toRoute('/create_party')">もくもく会を作成</a></p>
+      <p><a class="link" @click="toRoute('/party/created_list')">作成したもくもく会一覧</a></p>
+      <p><a class="link" @click="toRoute('/party/participated_list')">参加したもくもく会一覧</a></p>
+      <p><a class="link" @click="toRoute('/messages')">メッセージ(参加者側)</a></p>
+      <p><a class="link" @click="toRoute('/messages_for_leader')">メッセージ(主催者側)</a></p>
+      <p><a class="link" @click="toRoute('/user/show')">ユーザー情報</a></p>
+      <p><a class="link" @click="logout">ログアウト</a></p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -46,6 +69,19 @@ export default {
         })
         .catch((e) => console.log(e))
     },
+    toggleMenu() {
+      const menuContent = document.getElementById('menuContent');
+      if (menuContent.style.left === '0px') {
+        menuContent.style.left = '-100%'; // メニューを隠す
+      } else {
+        menuContent.style.left = '0px'; // メニューを表示する
+      }
+    },
+    toRoute(url) {
+      const menuContent = document.getElementById('menuContent');
+      menuContent.style.left = '-100%';
+      this.$router.push(url);
+    }
   },
 }
 </script>
@@ -59,7 +95,7 @@ export default {
   background-color: white;
   border-bottom: 1px solid black;
   margin-bottom: 100px;
-  z-index: 1;
+  z-index: 2;
   display: flex;
   align-items: center;
 }
@@ -75,15 +111,15 @@ nav {
 }
 
 .link {
-  color: #333; /* 色を黒っぽく設定 */
-  text-decoration: none; /* 下線を削除 */
-  transition: color ease; /* 色の遷移効果を追加 */
-  font-family: 'Helvetica', 'Arial', sans-serif; /* 任意のフォントを設定。適宜変更すること */
-  font-size: 18px; /* フォントサイズを設定。適宜変更すること */
-  font-weight: bold; /* テキストをボールドに設定 */
+  color: #333;
+  text-decoration: none;
+  transition: color ease;
+  font-family: 'Helvetica', 'Arial', sans-serif;
+  font-size: 18px;
+  font-weight: bold;
 }
 .link:hover{
-  color: #066aff; /* ホバー時の色を薄い黒に設定 */
+  color: #066aff;
 }
 button {
   background: none;
@@ -98,5 +134,70 @@ button {
   z-index: 100;
   background-color: white;
   border: 1px solid black;
+}
+
+/* header_sp用デザイン */
+.header_sp {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 100px;
+  background-color: white;
+  border-bottom: 1px solid black;
+  margin-bottom: 100px;
+  z-index: 2;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+}
+
+.hamburger-menu {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 30px;
+  height: 25px;
+  cursor: pointer;
+}
+
+/* ハンバーガーメニューのバー */
+.hamburger-menu .bar {
+  height: 3px;
+  background-color: black;
+}
+
+/* メニューコンテンツスタイル */
+.menu-content {
+  position: fixed;
+  top: 0;
+  left: -100%;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  z-index: 1;
+  overflow:scroll;
+  transition: left 0.3s;
+}
+
+/* メニューアイテムスタイル */
+.menu-content p {
+  height:50px;
+  padding: 10px;
+  border-bottom: 1px solid black;
+  text-decoration: none;
+  color: black;
+  z-index:1;
+}
+
+@media (min-width: 601px) {
+  .header_sp {
+    display: none;
+  }
+}
+
+@media (max-width: 600px) {
+  .header {
+    display: none;
+  }
 }
 </style>
